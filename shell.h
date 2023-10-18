fae181da96ee728b3f2c012d45909fc2729ff335
Kisakye19 <kisakyepesther@gmail.com>
2023-10-18 06:25:29 -0700

    corrections

--- shell.h
+++ shell.h
@@ -4,14 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 char *read_line(void);
-void handle_exit();
-char **split_line(char *line);
+char **split_line(char *line, char separator);
 int execute(char **args, char **environ);
+int execute_single_command(char **args);
 ssize_t custom_getline(char **lineptr, size_t *n);
-void execute_env();
-int split_input_into_commands(char *input, char *commands[], int max_commands);
 
 #endif /* SHELL_H */
-
